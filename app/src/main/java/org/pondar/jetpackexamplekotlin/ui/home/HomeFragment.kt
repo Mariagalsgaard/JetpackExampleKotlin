62c79793facd35364970ce9df2659708dcf729db
zaifrun <martinknud@gmail.com>
2020-10-01 13:45:30 +0200

    updated imports

--- app/src/main/java/org/pondar/jetpackexamplekotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/org/pondar/jetpackexamplekotlin/ui/home/HomeFragment.kt
@@ -7,11 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import org.pondar.jetpackexamplekotlin.R
-import org.pondar.jetpackexamplekotlin.ui.gallery.GalleryViewModel
 
 class HomeFragment : Fragment() {
 
